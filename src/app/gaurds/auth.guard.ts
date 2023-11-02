6f93ab503e1f53b4f4f6fd8179609b0d460cca03
Rakshitharaju125 <rakshuraju1997@gmail.com>
2023-11-02 21:39:29 +0530

    login

---
+++ src/app/gaurds/auth.guard.ts
@@ -0,0 +1,11 @@
+import { Inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  if(sessionStorage.getItem('email')) {
+    return true;
+  }else {
+    const router= Inject(Router);
+    return router.navigate(['login'])
+  }
+};
